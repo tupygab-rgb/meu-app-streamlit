8cfd93ca7f8a1597b1b982996f43f091a31a4b90
tupygab-rgb <tupygab@gmail.com>
2025-12-12 12:28:05 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ nome = st.text_input("Nome: ")
 idade = st.number_input("Idade: ", min_value=10, max_value=150, step=1.0)
 peso = st.number_input("Peso (kg): ", min_value=30, max_value=400, step=0.1)
 altura = st.number_input("Altura (cm): ", min_value=100, max_value=300, step=1.0)
-sexo = st.select_box("Sexo Biológico: ", ["Masculino", "Feminino"])
+sexo = st.selectbox("Sexo Biológico: ", ["Masculino", "Feminino"])
+
 
 
 
