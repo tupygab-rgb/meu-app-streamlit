9e729358bf467e5194f287bbb78feaec43578c96
tupygab-rgb <tupygab@gmail.com>
2025-12-12 13:11:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,13 +13,14 @@ altura = st.number_input("Altura (cm): ", min_value=100, max_value=300, step=1, 
 sexo = st.selectbox("Sexo Biológico: ", ["Masculino", "Feminino"])
 
 #Calcular TMB
-def calcular_tmb(idade, peso, altura, sexo)
-    if sexo = "Masculino"
-      tmb = 88.36 + (13.4*peso) + (4.8*altura) − (5.7*idade)
+def calcular_tmb(idade, peso, altura, sexo):
+    if sexo == "Masculino"
+      tmb = 88.36 + (13.4*peso) + (4.8*altura) - (5.7*idade)
     else:
-      tmb = 447.6 + (9.2*peso) + (3.1*altura) − (4.3*idade)
+      tmb = 447.6 + (9.2*peso) + (3.1*altura) - (4.3*idade)
     return tmb
 st.write("Sua Gasto Calórico Base é:", tmb)
+
 
 
 
