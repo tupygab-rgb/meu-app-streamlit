a7add6ab3b513c359654ed8135962d41c0db5985
tupygab-rgb <tupygab@gmail.com>
2025-12-12 12:29:47 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ st.header("Preencha seus dados abaixo para começar!")
 
 #Inputs
 nome = st.text_input("Nome: ")
-idade = st.number_input("Idade: ", min_value=10, max_value=150, step=1.0)
+idade = st.number_input("Idade: ", min_value=10, max_value=150, step=1)
 peso = st.number_input("Peso (kg): ", min_value=30, max_value=400, step=0.1)
 altura = st.number_input("Altura (cm): ", min_value=100, max_value=300, step=1.0)
 sexo = st.selectbox("Sexo Biológico: ", ["Masculino", "Feminino"])
+
 
 
 
