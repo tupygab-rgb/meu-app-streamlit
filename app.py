c9caa838e0332e7c7ff5d359abb9a80ce1d7040b
tupygab-rgb <tupygab@gmail.com>
2025-12-12 16:43:13 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.markdown("""
 🌿 **Alimentação Natural e Consciente**  
 Lembre-se: o que você ingere influencia seu bem-estar completo.  
 Procure priorizar alimentos naturais, frescos e equilibrados, criando uma rotina alimentar que respeite seu corpo e traga vitalidade para todas as áreas da sua vida.
-""")
+"Esta calculadora serve apenas como referência. O mais importante é escolher alimentos naturais e frescos, escutando seu corpo e cultivando vitalidade física e espiritual, respeitando a harmonia do seu ser."""")
 
 #Inputs
 nome = st.text_input("Nome: ")
@@ -89,6 +89,7 @@ Além disso, essa calculadora foi feita de forma amadora, sem qualquer formaçã
             st.markdown("""Mais importante do que contar calorias é ouvir seu corpo e escolher alimentos que promovam saúde e vitalidade.
             """)
     
+
 
 
 
